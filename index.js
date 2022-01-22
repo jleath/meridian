62cdfb2efd6d6d6af8928fe932cce2df51ba1ef0
Joshua Leath <lawnfurniture@icloud.com>
2022-01-22 11:57:45 -0500

    first version

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+console.log(process.env);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const db = require('./queries');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/',(_, response) => {
+  response.json({ info: 'Messier API starting point' });
+});
+
+app.get('/dsos', db.getDsos);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
---
+++ queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+const getDsos = (request, response) => {
+  pool.query('SELECT * FROM dsos', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getDsos,
+};
