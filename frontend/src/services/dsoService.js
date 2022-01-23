a339df2a2d2dc984e7c2f69a517d68b86ee649b2
Joshua Leath <lawnfurniture@icloud.com>
2022-01-23 16:49:44 -0500

    switch databases for dev or prod environment

--- frontend/src/services/dsoService.js
+++ frontend/src/services/dsoService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const DSO_DB_URL = 'http://localhost:3003/dsos';
+const DSO_DB_URL = 
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3003/dsos' : 'https://joshleath.com/dsos';
 
 const getDsos = async () => {
   const response = await axios.get(DSO_DB_URL);
